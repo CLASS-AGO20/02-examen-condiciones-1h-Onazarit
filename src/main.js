2a8cdd55a8e6b0a765e33dfa8401d817cad79867
Onazarit <onazarit@ucol.mx>
2020-11-26 15:27:35 -0600

    Implemented function convertir

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 export default class App {
 
+    convertir(pesos){
+        let conversion;
+        if(pesos < 2000){
+            conversion = pesos * 20.5;
+            return(conversion);
+        }
+        else if (pesos >= 2000){
+            conversion = pesos * 22.7;
+            return(conversion);
+        }
+    }
+
 }
 
 let app = new App();
+
+// Prueba metodo convertir
+console.log("1500 pesos equivalen a: "+app.convertir(1500));
+console.log("2000 pesos equivalen a: "+app.convertir(2000));
+console.log("3500 pesos equivalen a: "+app.convertir(3500));
