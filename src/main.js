623823d86c47a8b6e07b781d76f09af30bb0ee76
Onazarit <onazarit@ucol.mx>
2020-11-26 15:35:59 -0600

    Implemented function costoRenta

--- src/main.js
+++ src/main.js
@@ -12,6 +12,19 @@ export default class App {
         }
     }
 
+    costoRenta(kilometros){
+        let costo;
+        if(kilometros < 50)
+            costo = kilometros * 3;
+        else if((kilometros >= 50) && (kilometros < 100))
+            costo = kilometros * 5;
+        else if((kilometros >= 100) && (kilometros < 200))
+            costo = kilometros * 6;
+        else
+            costo = kilometros * 6.5;
+        return(costo);
+    }
+
 }
 
 let app = new App();
@@ -20,3 +33,9 @@ let app = new App();
 console.log("1500 pesos equivalen a: "+app.convertir(1500));
 console.log("2000 pesos equivalen a: "+app.convertir(2000));
 console.log("3500 pesos equivalen a: "+app.convertir(3500));
+
+// Prueba metodo costoRenta
+console.log("El auto rentado recorrio 40km, el costo es: "+app.costoRenta(40));
+console.log("El auto rentado recorrio 60km, el costo es: "+app.costoRenta(60));
+console.log("El auto rentado recorrio 100km, el costo es: "+app.costoRenta(100));
+console.log("El auto rentado recorrio 205km, el costo es: "+app.costoRenta(205));
